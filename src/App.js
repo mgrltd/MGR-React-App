a9dbb4da7392afea504f39e490d033bfade2effe
ramachandra.m <ramachandra.m@xcubelabs.com>
2023-07-27 10:30:59 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+
+      <div>
+      <div className="spinner-container">
+       <div className="spinner"></div>
+      <div className="saround">  <img alt='' src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQd7OiEKPujgXsrob_L8sL4M0SQdSEiGx2QxD_PfZ4qWA&s' height={50} width={50}         style={{ borderRadius: '50px' }} />  </div>
+    </div>
+     
+
+
+        <h1>MGR_Training_Institute </h1>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.facebook.com/profile.php?id=100094577346844"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Fallow MGR_Training_Institute Page
         </a>
+         </div>
       </header>
     </div>
   );
