6828a6dd9bb6b6d9f1a74977eb7bf2799233feca
ramachandra.m <ramachandra.m@xcubelabs.com>
2023-07-30 14:21:35 +0530

    secound commit

--- src/App.js
+++ src/App.js
@@ -1,31 +1,44 @@
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
+const App = () => {
+ const [isLoading, setIsLoading] = useState(true);
 
+
+ useEffect(() => {
+   setTimeout(() => {
+     setIsLoading(false);
+      }, 3000);
+ }, []);
+
+
+ return (
+     <div className="App">
+      {isLoading ?
       <div>
-      <div className="spinner-container">
-       <div className="spinner"></div>
-      <div className="saround">  <img alt='' src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQd7OiEKPujgXsrob_L8sL4M0SQdSEiGx2QxD_PfZ4qWA&s' height={50} width={50}         style={{ borderRadius: '50px' }} />  </div>
-    </div>
-     
+        <div className="spinner-container">
+          <div className="spinner"></div>
+          <div className="saround">  <img alt='MGR_icon' src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQd7OiEKPujgXsrob_L8sL4M0SQdSEiGx2QxD_PfZ4qWA&s' height={50} width={50}  style={{ borderRadius: '50px' }} />  </div>
+        </div>
+        <div>
+          <div>
+            <h1>Welcome </h1>
+            <h3>To</h3>
+            <h1>MGR_Training_Institute </h1>
+            <a className="App-link" href="https://www.facebook.com/profile.php?id=100094577346844"  >  Fallow MGR_Training_Institute Page </a>
+          </div>
+        </div>
+      </div>
+      :
+      <div>
+      </div>
+      }
+ 
+</div>
 
 
-        <h1>MGR_Training_Institute </h1>
-        <a
-          className="App-link"
-          href="https://www.facebook.com/profile.php?id=100094577346844"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Fallow MGR_Training_Institute Page
-        </a>
-         </div>
-      </header>
-    </div>
-  );
-}
+ );
+};
+
 
 export default App;
